1dfc5317e84745ecfc1e66685462c594ffed62be
Asteway Nigussie <asteway17@gmail.com>
2025-04-16 14:11:01 -0500

    added in the FullNameApplication

---
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+// Importing the scanner
+import java.util.Scanner;
+
+public class FullNameApplication {
+
+    public static void main(String[] args) {
+
+        // Initializing the scanner - called it cheese cuz why not :/
+        Scanner cheese = new Scanner(System.in);
+
+        // Requesting the name inputs
+        System.out.print("Please enter your name \nFirst name: ");
+        String firstName = cheese.nextLine();
+        System.out.print("Middle name: ");
+        String middleName = cheese.nextLine();
+        System.out.print("Last name: ");
+        String lastName = cheese.nextLine();
+        System.out.print("Suffix: ");
+        String suffix = cheese.nextLine();
+
+        // Adjusting for the condition if middle name and suffix are empty
+        middleName = middleName.trim().isEmpty() ? "" : " "+middleName.trim();
+        suffix = suffix.trim().isEmpty() ? "" : ", "+suffix.trim();
+
+        // Adding up the full name
+        String fullName = firstName.trim() + middleName +" "+ lastName.trim() + suffix;
+
+        // Printing it out
+        System.out.println("Full name: " + fullName);
+    }
+}
