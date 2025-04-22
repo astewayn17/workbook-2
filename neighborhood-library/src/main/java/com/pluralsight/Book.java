46a380dffd796e3dfa8f1f23bb92b88e1555fe6d
Asteway Nigussie <asteway17@gmail.com>
2025-04-22 18:45:52 -0500

    finished up neighborhood library

--- neighborhood-library/src/main/java/com/pluralsight/Book.java
+++ neighborhood-library/src/main/java/com/pluralsight/Book.java
@@ -1,14 +1,14 @@
 package com.pluralsight;
 public class Book {
 
-    //
+    // Creating private variables to make them unable to modify without getters
     private int id;
     private String isbn;
     private String title;
     private boolean isCheckedOut;
     private String checkedOutTo;
 
-    //
+    // Instantiating the variables into the constructor using parameters for three of them
     public Book (int id, String isbn, String title) {
         this.id = id;
         this.isbn = isbn;
@@ -16,40 +16,37 @@ public class Book {
         this.isCheckedOut = false;
         this.checkedOutTo = "";
     }
-
-    //
+    // Using getters to return the private variables
     public int getId() { return id; }
     public String getIsbn() { return isbn; }
     public String getTitle() { return title; }
     public boolean getIsCheckedOut() { return isCheckedOut; }
     public String getCheckedOutTo() { return checkedOutTo; }
 
-    //
-    public void checkOut(String name) {
+    // Checkout method to return if a book is already checked out or has been checked out
+    public String checkOut(String name) {
         if (isCheckedOut) {
-            System.out.println("This book is already checked out.\nPlease select another (A)" +
-                    "or come another day once it has been returned - exit (E). ");
+            return "This book is already checked out by " + checkedOutTo + ".";
         } else {
             isCheckedOut = true;
             checkedOutTo = name;
+            return "\"" + title + "\" successfully checked out to " + name + ".";
         }
     }
-
-    //
-    public void checkIn() {
+    // Check in method to verify if check in is done and if a book is already checked in
+    public String checkIn() {
         if (!isCheckedOut) {
-            System.out.println("You have entered an incorrect book that is already present." +
-                    "\nPlease confirm your book again ");
+            return "This book is already checked in.";
         } else {
             isCheckedOut = false;
             checkedOutTo = "";
+            return "\"" + title + "\" successfully checked in.";
         }
     }
+    // Simple way to use toString and print out the checked out book status along with a ternary if the book is gone
     public String toString() {
-        return "Book ID: " + id +
-                ", ISBN: " + isbn +
-                ", Title: \"" + title + "\"" +
-                (isCheckedOut ? " (Checked out to: " + checkedOutTo + ")" : " (Available)");
+        return "ID: " + id + ", ISBN: " + isbn + ", Title: \"" + title + "\"" +
+                (isCheckedOut ? " (Checked out to: " + checkedOutTo + ")" : "");
     }
 }
 
--- neighborhood-library/src/main/java/com/pluralsight/LibraryApp.java
+++ neighborhood-library/src/main/java/com/pluralsight/LibraryApp.java
@@ -6,14 +6,14 @@ public class LibraryApp {
 
         Scanner input = new Scanner(System.in);
 
+        // Starting off by making my array titled books
         Book[] books = new Book[20];
-
         books[0] = new Book(1, "978-1234567890", "The Holy Bible");
         books[1] = new Book(2, "978-0987654321", "The Holy Quran");
         books[2] = new Book(3, "978-8437076102", "The Origin of Species");
         books[3] = new Book(4, "978-1443434973", "1984");
         books[4] = new Book(5, "978-0140449143", "The Republic");
-        books[5] = new Book(6, "978-0517053614", "The Complete Works of Shakespeare");
+        books[5] = new Book(6, "978-0517053614", "The Complete Works of W.S.");
         books[6] = new Book(7, "978-0140447576", "The Communist Manifesto");
         books[7] = new Book(8, "978-0895267115", "Das Kapital");
         books[8] = new Book(9, "978-0937832387", "The Prince");
@@ -29,101 +29,111 @@ public class LibraryApp {
         books[18] = new Book(19, "978-0486821955", "Don Quixote");
         books[19] = new Book(20, "978-8560860271", "Crime and Punishment");
 
-        boolean accessing = true;
-        while (accessing) {
+        boolean accessing = true;  // Making a while loop with the boolean argument saying the library is being accessed
+        while (accessing) { // which introduces the home screen
             System.out.println("\nAsteway's Library of Knowledge\n======== Home Screen ========");
-            System.out.println("\nView Available Books ----- (B)");
+            System.out.println("\nView Available Books ----- (A)");
             System.out.println("View Checked Out Books --- (C)");
-            System.out.println("Exit --------------------- (E)");
+            System.out.println("Exit --------------------- (X)");
 
             System.out.print("Please enter your choice: ");
             String choice = input.nextLine().trim().toUpperCase();
             System.out.print("\n");
 
+            // Based on what the user entered as their choice in the menu, this switch statement will run through the
+            // cases and proceed as such
             switch (choice) {
 
-                case "B":
+                case "A": // Using a for each loop that is running through the book array with an if condition saying if
+                          // the book isn't checked out, that it should print
                     for (Book book : books) {
                         if (!book.getIsCheckedOut()) {
                             System.out.println(book);
                         }
-                }System.out.println("\nEnter the book ID to checkout or select (X) to return to the home screen. ");
+                } System.out.println("\nEnter the book ID to checkout or select (E) to return to the home screen. ");
+                    String inputId = input.nextLine().trim(); // Requesting the book ID as a string and
+                    // storing it in the scanner while outside spaces are removed and forced to uppercase
 
-                String inputId = input.nextLine().trim().toUpperCase();
-
-                try {
+                try { // Inputting all the following info into a try catch to make it still run in case of an error
                     int idToCheckOut = Integer.parseInt(inputId);
                     boolean bookFound = false;
 
-                    for (Book book : books) {
+                    for (Book book : books) {  // Running through the books array and using an if conditional
+                                               // with reference to the getter to set bookFound variable as true
                         if (book.getId() == idToCheckOut) {
                             bookFound = true;
 
-                            if (!book.getIsCheckedOut()) {
+                            if (!book.getIsCheckedOut()) { // If the book isn't checked out, it'll do the following
                                 System.out.println("Enter your name to check out the book: ");
+                                // Storing the username as an array to make the name into proper format
                                 String [] userName = input.nextLine().toLowerCase().trim().split("\\s+");
-                                String userNameInput = userName[0].substring(0,1).toUpperCase() + userName[0].substring(1)
-                                        + " " + userName[1].substring(0,1).toUpperCase() + userName[1].substring(1);
-                                book.checkOut(userNameInput);
+                                String userNameInput = userName[0].substring(0,1).toUpperCase() + userName[0]
+                                        .substring(1) + " " + userName[1].substring(0,1).toUpperCase()
+                                        + userName[1].substring(1);
+                                System.out.println(book.checkOut(userNameInput));
                             } else {
-                                System.out.println("That book is already checked out. ");
+                                System.out.println("Unfortunately \"" + book.getTitle() + "\" is already checked out.");
                             }
                             break;
                         }
                     }
-                    if (!bookFound) {
+                    if (!bookFound) { // If the book ID isn't correct, then it will print out the message
                         System.out.println("Book ID not found.");
                     }
-                } catch (NumberFormatException e) {
+                } catch (NumberFormatException e) {  // Catch statement to print out the invalid input message in case
+                                                     // anything is still inputted incorrectly
                     System.out.println("Invalid input! Please enter and valid book ID. ");
                 }
                 break;
 
-                case "C":
+                case "C": // Case C to test if a checked out book is listed
+                    boolean foundCheckedOut = false;
                     for (Book book : books) {
                         if (book.getIsCheckedOut()) {
                             System.out.println(book);
+                            foundCheckedOut = true;
                     }
                 }
-                    System.out.println("\nEnter the book ID to check in or select (X) to return to the home screen. ");
-                    String checkInInput = input.nextLine().trim().toUpperCase();
+                    if (!foundCheckedOut) {  // If not, it will print the following statement
+                        System.out.println("There are no books currently checked out.\nPlease select (A) or (X).");
+                        break;
+                    }  // Also asks if the user wants to check in a book
+                    System.out.println("\nEnter the book ID to check in or select (X) to exit to the home screen. ");
+                    String checkInInput = input.nextLine().trim();
 
-                    try {
-                        int idToCheckIn = Integer.parseInt(checkInInput);
+                    try { // Starting another try catch
+                        int idToCheckIn = Integer.parseInt(checkInInput); // Converting the ID to an integer input
                         boolean bookFound = false;
 
-                        for (Book book : books) {
-                            if (book.getId() == idToCheckIn) {
-                                bookFound = true;
+                        for (Book book : books) { // Runs through the book array as a for each loop
+                            if (book.getId() == idToCheckIn) {  // If the book ID is equal to the ID of the book
+                                bookFound = true;               // being checked in, then the bookFound is true
 
-                                if (book.getIsCheckedOut()) {
-                                    book.checkIn();
-                                    System.out.println("Book successfully checked in!");
+                                if (book.getIsCheckedOut()) { // Successfully checks in the book
+                                    System.out.println(book.checkIn());
                                 } else {
-                                    System.out.println("That book is already checked in!");
+                                    System.out.println("That book is already checked in! Back to home screen.");
                                 }
                                 break;
                             }
                         }
-                        if (!bookFound) {
+                        if (!bookFound) { // If this is false, it will say it isn't found
                             System.out.println("Book ID not found.");
                         }
 
-                    } catch (NumberFormatException e) {
+                    } catch (NumberFormatException e) { // Catches any further invalid inputs for the ID
                         System.out.println("Invalid input! Please enter a valid book ID.");
                     }
                     break;
 
-                case "E":
+                case "X": // This is the exit option and says accessing is now false so it will break the loop
                     accessing = false;
-                    System.out.println("Thank you for visiting!");
+                    System.out.println("Thank you for visiting and have a nice day!");
                     break;
                 default:
                     System.out.println("Invalid choice! Try again.");
             }
         }
-
-
     }
 }
 
